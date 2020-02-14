520d5be35ec743bc168b74cb6b96851d5ab56932
Rajendrasaud <saudrajiv10@gmail.com>
2020-02-14 13:40:25 +0545

    cart table modified

---
+++ model/cart.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Cart = mongoose.model('Cart', {
+    user_id: {
+        type: String
+    },
+    product_name: {
+        type: String
+    },
+    product_image: {
+        type: String
+    },
+    product_price: {
+        type: String
+    },
+    product_brand: {
+        type: String
+    }
+})
+module.exports = Cart
